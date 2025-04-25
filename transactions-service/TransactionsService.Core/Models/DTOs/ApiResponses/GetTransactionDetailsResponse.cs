80ded135c8c771f40a816d6837e08c937e34756d
olumuyiwa-agboola <agboolaod@gmail.com>
2025-04-25 19:01:25 +0100

    Adjust GetTransactionDetailsResponse

--- transactions-service/TransactionsService.Core/Models/DTOs/ApiResponses/GetTransactionDetailsResponse.cs
+++ transactions-service/TransactionsService.Core/Models/DTOs/ApiResponses/GetTransactionDetailsResponse.cs
@@ -7,10 +7,10 @@ namespace TransactionsService.Core.Models.DTOs.ApiResponses
     {
         public decimal Amount { get; init; }
         public string Remarks { get; init; }
-        public Currency Currency { get; init; }
-        public DateTime DateTime { get; init; }
-        public string SenderName { get; init; }
+        public string DateTime { get; init; }
         public string Reference { get; init; }
+        public Currency Currency { get; init; }
+        public string SenderName { get; init; }
         public string SenderBankName { get; init; }
         public string BeneficiaryName { get; init; }
         public TransactionStatus Status { get; init; }
@@ -24,13 +24,13 @@ namespace TransactionsService.Core.Models.DTOs.ApiResponses
             Amount = transaction.Amount;
             Remarks = transaction.Remarks;
             Currency = transaction.Currency;
-            DateTime = transaction.DateTime;
             Reference = transaction.Reference;
             SenderName = transaction.SenderName;
             SenderBankName = transaction.SenderBankName;
             BeneficiaryName = transaction.BeneficiaryName;
             SenderAccountNumber = transaction.SenderAccountNumber;
             BeneficiaryBankName = transaction.BeneficiaryBankName;
+            DateTime = transaction.DateTime.ToString("yyyy-MM-dd HH:mm:ss");
             BeneficiaryAccountNumber = transaction.BeneficiaryAccountNumber;
         }
     }
